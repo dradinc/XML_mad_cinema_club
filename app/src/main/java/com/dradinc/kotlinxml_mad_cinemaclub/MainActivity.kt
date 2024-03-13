9084f9b9c7bf2d0778abecfc8155c7efbb84bfec
dradinc <dradinc@yandex.ru>
2024-03-13 13:08:30 +0300

    add navigation

--- app/src/main/java/com/dradinc/kotlinxml_mad_cinemaclub/MainActivity.kt
+++ app/src/main/java/com/dradinc/kotlinxml_mad_cinemaclub/MainActivity.kt
@@ -3,9 +3,17 @@ package com.dradinc.kotlinxml_mad_cinemaclub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.dradinc.kotlinxml_mad_cinemaclub.helpers.SharedPreferencesHelper
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // Объявляем переменную для контролера навигации
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +26,11 @@ class MainActivity : AppCompatActivity() {
             val onBoardingActivity = Intent(this@MainActivity, OnBoarding::class.java)
             startActivity(onBoardingActivity)
         }
+
+        // Делаем так, чтобы переключались фрагменты
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(findViewById<BottomNavigationView>(R.id.bottomNavView), navController)
 
     }
 }
